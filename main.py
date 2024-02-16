ced96e7930934ca99589c4a824494c4aa7e5c281
valeri245195 <149378757+valeri245195@users.noreply.github.com>
2024-02-16 14:32:18 +0200

    Forst Commit

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("go")
+from datetime import datetime
+def days_to_birthday(birthday):
+    if birthday:
+        today = datetime.now().date()
+        next_birthday_year = today.year
+        birthday_this_year = datetime.strptime(birthday, '%Y-%m-%d').date().replace(year=next_birthday_year)
+        if today > birthday_this_year:
+            next_birthday_year += 1
+            birthday_this_year = birthday_this_year.replace(year=next_birthday_year)
+        days_left = (birthday_this_year - today).days
+        return days_left
+    else:
+        return None
+# today = datetime.now().date()
+# print(today)
+# days_left = days_to_birthday('1990-02-17')
+# print(days_left)
